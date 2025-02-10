9dbded1da27346f99fe87f3d963e545ece8f5dd8
navneet451 <navneetthakur451@gmail.com>
2025-02-11 00:17:11 +0530

    Updated API URL for Render deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy:{
-      "/api":{
-        target:"http://localhost:3000",
-        secure:false,
+    proxy: {
+      "/api": {
+        target: "https://blog-app-bjxb.onrender.com", // Use the deployed backend URL
+        changeOrigin: true,
+        secure: true,
       },
     },
   },
-  plugins: [react(),
-  ],
-})
+  plugins: [react()],
+});
