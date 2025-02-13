2e70a5fb0928186765075764d8db97cc215c2a61
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2025-02-13 23:13:44 +0530

    Update index.js with frontend url as i upload client on versel

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "https://blog-app-2-hh1j.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://my-repository-three-blue.vercel.app", credentials: true }));
 
 
 app.get("/", (req, res) => {
