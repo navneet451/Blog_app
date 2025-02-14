d423c2a17a8cc60e582d4abb3cad1d356a74f146
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2025-02-14 17:47:29 +0530

    Update vite.config.js removing proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://blog-app-backend-o0wt.onrender.com", // Use the deployed backend URL
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
   plugins: [react()],
 });
