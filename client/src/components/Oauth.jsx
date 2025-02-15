af3b8ac98ea210c3764e87436e01b55eb53e7608
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2025-02-15 15:49:54 +0530

    Update Oauth.jsx

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -1,57 +1,57 @@
-import React from "react";
-import { Button } from "flowbite-react";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
-import { app } from "../firebase";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { signInSuccess } from "../redux/user/userSlice";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+// import React from "react";
+// import { Button } from "flowbite-react";
+// import { AiFillGoogleCircle } from "react-icons/ai";
+// import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+// import { app } from "../firebase";
+// import axios from "axios";
+// import { useDispatch } from "react-redux";
+// import { signInSuccess } from "../redux/user/userSlice";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
 
-const Oauth = () => {
-  const auth = getAuth(app);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+// const Oauth = () => {
+//   const auth = getAuth(app);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
 
 
-  const handleGoogleClick = async () => {
-    const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({ prompt: "select_account" });
-    try {
-      const resultFromGoogle = await signInWithPopup(auth, provider);
-    //   console.log(resultFromGoogle);
-      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/google`, {
-        name: resultFromGoogle.user.displayName,
-        email: resultFromGoogle.user.email,
-        googlePhotoUrl: resultFromGoogle.user.photoURL,
-      }, {
-        withCredentials: true,
-      });
-      console.log(res);
+//   const handleGoogleClick = async () => {
+//     const provider = new GoogleAuthProvider();
+//     provider.setCustomParameters({ prompt: "select_account" });
+//     try {
+//       const resultFromGoogle = await signInWithPopup(auth, provider);
+//     //   console.log(resultFromGoogle);
+//       const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/google`, {
+//         name: resultFromGoogle.user.displayName,
+//         email: resultFromGoogle.user.email,
+//         googlePhotoUrl: resultFromGoogle.user.photoURL,
+//       }, {
+//         withCredentials: true,
+//       });
+//       console.log(res);
       
-      if (res.status === 200) {
-        dispatch(signInSuccess(res.data));
-        toast.success(res.data.message);
-        navigate("/");
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error(error.response?.data?.message);
-    }
-  };
+//       if (res.status === 200) {
+//         dispatch(signInSuccess(res.data));
+//         toast.success(res.data.message);
+//         navigate("/");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       toast.error(error.response?.data?.message);
+//     }
+//   };
 
-  return (
-    <Button
-      type="button"
-      gradientDuoTone="pinkToOrange"
-      outline
-      onClick={handleGoogleClick}
-    >
-      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
-      Continue with google
-    </Button>
-  );
-};
+//   return (
+//     <Button
+//       type="button"
+//       gradientDuoTone="pinkToOrange"
+//       outline
+//       onClick={handleGoogleClick}
+//     >
+//       <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+//       Continue with google
+//     </Button>
+//   );
+// };
 
-export default Oauth;
+// export default Oauth;
